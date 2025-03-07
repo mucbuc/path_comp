8b43eb365d1becc92b21b871ea852c2f0781ffca
mucbuc <mucbuc@mucbucs-Mac-mini-2.local>
2025-03-07 12:41:12 -0800

    simplify by removing builder class and html svg maker

--- src/impl.hpp
+++ src/impl.hpp
@@ -1,109 +1,30 @@
-#pragma mark Vector_Builder
 
-template <int>
-struct Vector_Builder;
+#pragma mark Loop Impl
 
-template <>
-struct Vector_Builder<2> {
-    template <class T>
-    static std::array<T, 2> make_vector(T* p)
-    {
-        return { p[0], p[1] };
-    }
-
-    template <class T>
-    static std::ostream& write_vector(T p, std::ostream& o)
-    {
-        o << p[0] << " " << p[1];
-        return o;
-    }
-};
-
-template <>
-struct Vector_Builder<3> {
-    template <class T>
-    static std::array<T, 3> make_vector(T* p)
-    {
-        return { p[0], p[1], p[2] };
-    }
-
-    template <class T>
-    static std::ostream& write_vector(T p, std::ostream& o)
-    {
-        o << p[0] << " " << p[1] << " " << p[2];
-        return o;
-    }
-};
-
-#pragma mark Builder::PathDataPimpl
-template <typename scalar_type, int rank, typename index_type>
-struct Builder<scalar_type, rank, index_type>::PathDataPimpl {
-    void append(const Loop& l, index_type);
-
-    template <typename T, typename U, typename V>
-    void traverse(T on_loop_begin, U on_segment_end, V on_loop_end) const;
-
-    std::vector<vector_type> m_points;
-    std::vector<index_type> m_segments;
-    std::vector<index_type> m_loops;
-    std::vector<index_type> m_indecies;
-};
-
-#pragma mark Builder::Loop Impl
-
-template <typename scalar_type, int rank, typename index_type>
-Builder<scalar_type, rank, index_type>::Loop::Loop(scalar_type* begin)
+template <typename Vector_t, typename Index_t>
+Loop<Vector_t, Index_t>::Loop(vector_type begin)
+    : m_points(1, begin)
+    , m_segments()
 {
-    m_points.push_back(Vector_Builder<rank>::make_vector(begin));
 }
 
-template <typename scalar_type, int rank, typename index_type>
-Builder<scalar_type, rank, index_type>::Loop::Loop(vector_type begin)
-{
-    m_points.push_back(begin);
-}
-
-template <typename scalar_type, int rank, typename index_type>
-auto Builder<scalar_type, rank, index_type>::Loop::line(scalar_type* dest) -> Loop&
-{
-    segment_end(Vector_Builder<rank>::make_vector(dest));
-    return *this;
-}
-
-template <typename scalar_type, int rank, typename index_type>
-auto Builder<scalar_type, rank, index_type>::Loop::line(vector_type dest) -> Loop&
+template <typename Vector_t, typename Index_t>
+auto Loop<Vector_t, Index_t>::line(vector_type dest) -> Loop&
 {
     segment_end(dest);
     return *this;
 }
 
-template <typename scalar_type, int rank, typename index_type>
-auto Builder<scalar_type, rank, index_type>::Loop::curve(scalar_type* control, scalar_type* dest) -> Loop&
-{
-    m_points.push_back(Vector_Builder<rank>::make_vector(control));
-    segment_end(Vector_Builder<rank>::make_vector(dest));
-    return *this;
-}
-
-template <typename scalar_type, int rank, typename index_type>
-auto Builder<scalar_type, rank, index_type>::Loop::curve(vector_type control, vector_type dest) -> Loop&
+template <typename Vector_t, typename Index_t>
+auto Loop<Vector_t, Index_t>::curve(vector_type control, vector_type dest) -> Loop&
 {
     m_points.push_back(control);
     segment_end(dest);
     return *this;
 }
 
-template <typename scalar_type, int rank, typename index_type>
-auto Builder<scalar_type, rank, index_type>::Loop::curve(scalar_type* control1, scalar_type* control2, scalar_type* dest) -> Loop&
-{
-    m_points.push_back(Vector_Builder<rank>::make_vector(control1));
-    m_points.push_back(Vector_Builder<rank>::make_vector(control2));
-    segment_end(Vector_Builder<rank>::make_vector(dest));
-    return *this;
-}
-
-template <typename scalar_type, int rank, typename index_type>
-auto Builder<scalar_type, rank, index_type>::Loop::curve(vector_type control1, vector_type control2, vector_type dest) -> Loop&
+template <typename Vector_t, typename Index_t>
+auto Loop<Vector_t, Index_t>::curve(vector_type control1, vector_type control2, vector_type dest) -> Loop&
 {
     m_points.push_back(control1);
     m_points.push_back(control2);
@@ -111,119 +32,21 @@ auto Builder<scalar_type, rank, index_type>::Loop::curve(vector_type control1, v
     return *this;
 }
 
-template <typename scalar_type, int rank, typename index_type>
-Span<scalar_type> Builder<scalar_type, rank, index_type>::Loop::points() const
+template <typename Vector_t, typename Index_t>
+auto Loop<Vector_t, Index_t>::points() const -> std::vector<vector_type>
 {
-    return { m_points.data()->data(), m_points.size() * rank };
+    return m_points;
 }
 
-template <typename scalar_type, int rank, typename index_type>
-Span<index_type> Builder<scalar_type, rank, index_type>::Loop::segments() const
+template <typename Vector_t, typename Index_t>
+auto Loop<Vector_t, Index_t>::segments() const -> std::vector<index_type>
 {
-    return { m_segments.data(), m_segments.size() };
+    return m_segments;
 }
 
-template <typename scalar_type, int rank, typename index_type>
-void Builder<scalar_type, rank, index_type>::Loop::segment_end(vector_type dest)
+template <typename Vector_t, typename Index_t>
+void Loop<Vector_t, Index_t>::segment_end(vector_type dest)
 {
     m_points.push_back(dest);
     m_segments.push_back(m_points.size());
-}
-
-#pragma mark Builder Impl
-
-template <typename scalar_type, int rank, typename index_type>
-Builder<scalar_type, rank, index_type>::Builder()
-    : m_path_data(std::make_shared<PathDataPimpl>())
-{
-}
-
-template <typename scalar_type, int rank, typename index_type>
-void Builder<scalar_type, rank, index_type>::PathDataPimpl::append(const Loop& l, index_type index)
-{
-    const auto points_base { m_points.size() };
-    m_points.insert(m_points.end(), l.m_points.cbegin(), l.m_points.cend());
-    m_indecies.push_back(index);
-    auto new_entry { m_segments.insert(m_segments.end(), l.m_segments.cbegin(), l.m_segments.cend()) };
-
-    for (; new_entry != m_segments.end(); ++new_entry) {
-        *new_entry += points_base;
-    }
-    m_loops.push_back(m_segments.size());
-}
-
-template <typename scalar_type, int rank, typename index_type>
-void Builder<scalar_type, rank, index_type>::append(const Loop& l, index_type index)
-{
-    m_path_data->append(l, index);
-}
-
-template <typename scalar_type, int rank, typename index_type>
-void Builder<scalar_type, rank, index_type>::append(const Loop& l)
-{
-    append(l, index_type(m_path_data->m_loops.size()));
-}
-
-template <typename scalar_type, int rank, typename index_type>
-Span<scalar_type> Builder<scalar_type, rank, index_type>::points() const
-{
-    return { m_path_data->m_points.data()->data(), m_path_data->m_points.size() };
-}
-
-template <typename scalar_type, int rank, typename index_type>
-Span<index_type> Builder<scalar_type, rank, index_type>::segments() const
-{
-    return { m_path_data->m_segments.data(), m_path_data->m_segments.size() };
-}
-
-template <typename scalar_type, int rank, typename index_type>
-Span<index_type> Builder<scalar_type, rank, index_type>::loops() const
-{
-    return { m_path_data->m_loops.data(), m_path_data->m_loops.size() };
-}
-
-template <typename scalar_type, int rank, typename index_type>
-auto Builder<scalar_type, rank, index_type>::indecies() const -> Span<index_type>
-{
-    return { m_path_data->m_indecies.data(), m_path_data->m_indecies.size() };
-}
-
-template <typename scalar_type, int rank, typename index_type>
-template <typename T, typename U, typename V>
-void Builder<scalar_type, rank, index_type>::PathDataPimpl::traverse(T on_loop_begin, U on_segment_end, V on_loop_end) const
-{
-
-    ASSERT(m_indecies.size() == m_loops.size());
-
-    for (
-        index_type loop_index = 0, segment_index = 0, point_index = 0;
-        loop_index < m_loops.size();
-        ++loop_index) {
-
-        on_loop_begin(point_index, m_indecies[loop_index]);
-
-        const index_type point_begin { point_index };
-        ++point_index;
-        for (; segment_index != m_loops[loop_index]; ++segment_index) {
-
-            const auto segment_end = m_segments[segment_index];
-            on_segment_end(point_index, segment_end);
-            point_index = segment_end;
-        }
-
-        on_loop_end(point_begin);
-    }
-}
-
-template <typename scalar_type, int rank, typename index_type>
-template <typename T, typename U, typename V>
-void Builder<scalar_type, rank, index_type>::traverse(T on_loop_begin, U on_segment_end, V on_loop_end) const
-{
-    m_path_data->traverse(on_loop_begin, on_segment_end, on_loop_end);
-}
-
-template <typename scalar_type, int rank, typename index_type>
-std::ostream& Builder<scalar_type, rank, index_type>::write_point_at(size_t index, std::ostream& out) const
-{
-    return Vector_Builder<rank>::write_vector(m_path_data->m_points[index].data(), out);
 }
--- test/src/example.cpp
+++ test/src/example.cpp
@@ -1,117 +1,29 @@
 #include <iostream>
 #include <sstream>
 
-#include <cassert>
-#define ASSERT(p) assert((p))
-
-#include <path_comp/src/interface.hpp>
+#include <array> 
 #include <asserter/src/test.hpp>
+#include <path_comp/src/interface.hpp>
 
 using namespace std;
 
-using builder_type = path_comp::Builder<float, 2>;
-using Loop = builder_type::Loop;
-using vt = std::array<float, 2>;
-
-void test_loop()
-{
-    builder_type b;
-
-    b.append(Loop(vt { { 11, 12 } }.data())
-                 .line(vt { { 13, 14 } }.data())
-                 .line(vt { { 15, 16 } }.data())
-                 .curve(vt { { 17, 18 } }.data(), vt { { 19, 20 } }.data())
-                 .curve(vt { { 21, 22 } }.data(), vt { { 23, 24 } }.data(), vt { { 25, 26 } }.data()));
-
-    b.append(Loop(vt { { 1, 2 } }.data())
-                 .line(vt { { 3, 4 } }.data())
-                 .curve(vt { { 5, 6 } }.data(), vt { { 7, 8 } }.data(), vt { { 9, 10 } }.data())
-                 .line(vt { { 11, 12 } }.data()),
-        21);
-
-    cout << make_html_path(b, "triangle_") << std::endl;
-}
-
-static void test_svg()
-{
-    builder_type b;
-
-    b.append(Loop(vt { { 30, 50 } }.data())
-                 .line(vt { { 10, 60 } }.data())
-                 .line(vt { { 10, 30 } }.data()));
-
-    cout << make_html_path(b, "triangle_") << std::endl;
-}
-
-static void test_new_API()
-{
-    builder_type b;
-
-    b.append(Loop(vt { { 30, 50 } }.data())
-                 .line(vt { { 10, 60 } }.data())
-                 .curve(vt { { 50, 20 } }.data(), vt { { 100, 100 } }.data())
-                 .curve(vt { { 50, 20 } }.data(), vt { { 100, 100 } }.data(), vt { { 100, 100 } }.data())
-                 .line(vt { { 10, 30 } }.data()));
-
-    b.append(Loop(vt { { 30, 50 } }.data())
-                 .line(vt { { 10, 30 } }.data())
-                 .line(vt { { 10, 30 } }.data()));
-
-    b.traverse(
-        [](auto point_index, auto index) {
-            std::cout << "loop_begin " << index << ": " << point_index << std::endl;
-        },
-        [](auto segment_begin, auto segment_end) {
-            std::cout << "segment_end " << segment_begin << " " << segment_end << std::endl;
-        },
-        [](auto point_index) {
-            std::cout << "loop_end " << point_index << std::endl;
-        });
-}
-
-static void test_non_pointer_API()
-{
-    builder_type b;
-
-    b.append(Loop({ 30, 50 })
-                 .line({ 10, 60 })
-                 .curve({ 50, 20 }, { 100, 100 })
-                 .curve({ 50, 20 }, { 100, 100 }, { 100, 100 })
-                 .line({ 10, 30 }));
-
-    b.traverse(
-        [](auto point_index, auto index) {
-            std::cout << "loop_begin " << index << ": " << point_index << std::endl;
-        },
-        [](auto segment_begin, auto segment_end) {
-            std::cout << "segment_end " << segment_begin << " " << segment_end << std::endl;
-        },
-        [](auto point_index) {
-            std::cout << "loop_end " << point_index << std::endl;
-        });
-
-    std::cout << __PRETTY_FUNCTION__ << std::endl;
-}
+using loop_type = path_comp::Loop<std::array<float, 2>>;
 
 static void test_points_and_segments()
 {
-    builder_type b;
-
-    b.append(Loop({ 30, 50 })
+    auto b = loop_type({ 30, 50 })
                  .line({ 10, 60 })
                  .curve({ 50, 20 }, { 100, 100 })
                  .curve({ 50, 20 }, { 100, 100 }, { 100, 100 })
-                 .line({ 10, 30 }));
+                 .line({ 10, 30 });
 
-    ASSERT( b.points().size == 8 );
-    ASSERT( b.segments().size == 4 );
+    ASSERT(b.points().size() == 8);
+    ASSERT(b.segments().size() == 4);
 
+    auto c = loop_type({ 1, 1 });
 
-    builder_type c;
-    c.append(Loop({1, 1}));
-
-    ASSERT( c.points().size == 1 );
-    ASSERT( c.segments().size == 0 );
+    ASSERT(c.points().size() == 1);
+    ASSERT(c.segments().size() == 0);
 
     FOOTER;
 }
@@ -119,9 +31,5 @@ static void test_points_and_segments()
 int main()
 {
     test_points_and_segments();
-    test_non_pointer_API();
-    test_new_API();
-    test_loop();
-    test_svg();
     return 0;
 }
